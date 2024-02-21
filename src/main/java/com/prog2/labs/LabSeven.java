6829b3e850174b186267d83c93c59cb7bdce1833
Amir-Georges Haya <2286756@edu.vaniercollege.qc.ca>
2024-02-21 15:01:41 -0500

    testing a push

--- src/main/java/com/prog2/labs/LabSeven.java
+++ src/main/java/com/prog2/labs/LabSeven.java
@@ -4,22 +4,23 @@ package com.prog2.labs;
  * @author adinashby
  *
  */
-
 public class LabSeven {
 
-	/**
-	 * Write your test code below in the main (optional).
-	 *
-	 */
-	public static void main(String[] args) {
+    /**
+     * Write your test code below in the main (optional).
+     *
+     */
+    public static void main(String[] args) {
+        // this is a test
+    }
 
-	}
-
-	/**
-	 * Please refer to the README file for question(s) description
-	 */
-
-	public static int kthSmallestSubarraySum(int[] nums, int k) {
-		return 0;
-	}
+    /**
+     * Please refer to the README file for question(s) description
+     * @param nums
+     * @param k
+     * @return 
+     */
+    public static int kthSmallestSubarraySum(int[] nums, int k) {
+        return 0;
+    }
 }
